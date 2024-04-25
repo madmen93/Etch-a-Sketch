13b0d06ebb59888d94b467d001fceca6e6989181
Monica Daza <158379665+madmen93@users.noreply.github.com>
2024-04-24 23:09:56 -0500

    Fix the function to make a dinamic grid

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
-//Create the grid
 const container = document.querySelector('#container');
+const sizeButton = document.querySelector('#size');
 
+
+//Create the grid
 let n = 16;
-const grid = n * n;
-
+let grid = n;
 for (let i = 0; i < grid; i++) {
-    const square = document.createElement('div');
-    square.setAttribute('class', 'square');
-    container.appendChild(square);
-    
+    const row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    container.appendChild(row);
+    for (let j = 0; j < grid; j++) {
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        row.appendChild(square);
+    }        
+}
+
+/*const square = document.querySelector('.square');
+container.appendChild(square);        
+*/
+
+function chooseSize(){
+    num = prompt("Please enter a number between 1 to 100:");
+    n = Number(num);  
+    modificateSize();
+    return n;
+}
+
+function modificateSize(){
+    grid = n;
+    removeChildren();
+    for (let i = 0; i < grid; i++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        container.appendChild(row);
+        for (let j = 0; j < grid; j++) {
+            const square = document.createElement('div');
+            square.setAttribute('class', 'square');
+            row.appendChild(square);
+        }        
+    }
+}
+
+function removeChildren(){
+    let rows = document.getElementsByClassName('row');
+    let squares = document.getElementsByClassName('square');
+    for (let j = rows.length-1; j >= 0; j--) {
+        let square = squares[j];
+        square.remove();
+        let row = rows[j];
+        row.remove();
+    }
 }
 
 
+sizeButton.addEventListener("click", chooseSize);
 
 
 
